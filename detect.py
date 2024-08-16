e49e5844ed662cd7d57c76440823782a823c4ab8
KeSuarezAr <kesuarezar@uide.edu.ec>
2024-08-16 15:29:10 -0500

    change

--- detect.py
+++ detect.py
@@ -3,7 +3,8 @@ import joblib
 loaded_model = joblib.load("models/spam_model.pkl")
 loaded_vectorizer = joblib.load("models/vectorizer.pkl")
 
-email = ["500 Nuevos móviles desde 2004, MUST GO! Txt: NOKIA a No: 89545 y recoger el tuyo hoy!From ONLY 1 www.4-tc.biz 2optout 087187262701.50gbp/mtmsg18"]
+email = ["500 Nuevos móviles desde 2004, MUST GO! Txt: NOKIA a No: 89545 y recoger el tuyo hoy!From ONLY 1 "
+         "www.4-tc.biz 2optout 087187262701.50gbp/mtmsg18"]
 email = loaded_vectorizer.transform(email)
 
 prediction = loaded_model.predict(email)
